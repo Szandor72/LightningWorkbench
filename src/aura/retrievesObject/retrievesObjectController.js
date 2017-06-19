1a4cc5490c3d5741066123425b6c0a2ab2108d76
Christian Szandor Knapp <szandor72@gmail.com>
2017-06-19 18:30:00 +0200

    - converted dx source back to mdapi format

--- src/aura/retrievesObject/retrievesObjectController.js
+++ src/aura/retrievesObject/retrievesObjectController.js
@@ -3,5 +3,22 @@
         var recordIdChange = $A.get("e.c:retrievesObjectEvent");
         recordIdChange.setParam("recordId", component.get("v.recordId"));
         recordIdChange.fire();
+    },
+    
+    openModal : function(component, event, helper) {
+        var modal = component.find("modal");
+        modal.set("v.size", "large");
+        modal.set("v.active", true);
+    },
+    
+    gotoResults : function(component, event, helper) {
+        var evt = $A.get("e.force:navigateToComponent");
+        evt.setParams({
+            componentDef : "c:retrievesObjectResult",
+            componentAttributes: {
+                recordId : component.get("v.revordId")
+            }
+        });
+        evt.fire();
     }
 })
