a0557b31a4d47d215a935e36abe94021743ffc5b
Christian Szandor Knapp <szandor72@gmail.com>
2019-05-12 10:55:55 +0200

    added empAPI.isEMPEnabled

--- lwb/LightningWorkbench/lexAPIAccess/aura/lightningAPI/lightningAPIController.js
+++ lwb/LightningWorkbench/lexAPIAccess/aura/lightningAPI/lightningAPIController.js
@@ -24,39 +24,46 @@
   onInit: function(component, event, helper) {
     var empApi = component.find("empAPI");
     var replayId = -1;
-    console.log("twf");
-    // Subscribe to an event
-    empApi
-      .subscribe(
-        "/event/Response__e",
-        replayId,
-        $A.getCallback(function(eventReceived) {
-          console.log("Received event ", JSON.stringify(eventReceived));
-          var response =
-            eventReceived.data.payload.Payload__c +
-            eventReceived.data.payload.continuedPayload1__c +
-            eventReceived.data.payload.continuedPayload2__c +
-            eventReceived.data.payload.continuedPayload3__c +
-            eventReceived.data.payload.continuedPayload4__c;
-          component.set("v.response", response);
-        })
-      )
-      .then(function(subscription) {
-        // Confirm that we have subscribed to the event channel.
-        // We haven't received an event yet.
-        console.log("Subscribed to channel ", subscription.channel);
-        // Save subscription to unsubscribe later
-        component.set("v.subscription", subscription);
-      });
+    empApi.isEmpEnabled().then(
+      $A.getCallback(function(isEmpAPIEnabled) {
+        if (isEmpAPIEnabled) {
+          console.log("EmpAPI enabled");
+          empApi
+            .subscribe(
+              "/event/Response__e",
+              replayId,
+              $A.getCallback(function(eventReceived) {
+                console.log("Received event ", JSON.stringify(eventReceived));
+                var response =
+                  eventReceived.data.payload.Payload__c +
+                  eventReceived.data.payload.continuedPayload1__c +
+                  eventReceived.data.payload.continuedPayload2__c +
+                  eventReceived.data.payload.continuedPayload3__c +
+                  eventReceived.data.payload.continuedPayload4__c;
+                component.set("v.response", response);
+              })
+            )
+            .then(function(subscription) {
+              // Confirm that we have subscribed to the event channel.
+              // We haven't received an event yet.
+              console.log("Subscribed to channel ", subscription.channel);
+              // Save subscription to unsubscribe later
+              component.set("v.subscription", subscription);
+            });
 
-    // Uncomment below line to enable debug logging (optional)
-    // empApi.setDebugFlag(true);
+          // Uncomment below line to enable debug logging (optional)
+          // empApi.setDebugFlag(true);
 
-    // Register error listener and pass in the error handler function
-    empApi.onError(
-      $A.getCallback(function(error) {
-        // Error can be any type of error (subscribe, unsubscribe...)
-        console.error("EMP API error: ", JSON.stringify(error));
+          // Register error listener and pass in the error handler function
+          empApi.onError(
+            $A.getCallback(function(error) {
+              // Error can be any type of error (subscribe, unsubscribe...)
+              console.error("EMP API error: ", JSON.stringify(error));
+            })
+          );
+        } else {
+          console.log("EMP API not (yet) enabled");
+        }
       })
     );
   }
